72378d669b7b10eea65067b4986dbcb934fb89dd
idrissou <idrissoufaysal93@gmail.com>
2024-10-28 12:45:24 +0000

    end

--- src/components/shared/Details.tsx
+++ src/components/shared/Details.tsx
@@ -1,18 +1,13 @@
-import { Movie } from '@/lib/types';
-import { clearSelectedMovie } from '@/store/moviesSlice';
 import { RootState } from '@/store/store';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 
 export default function Details({
     close,
-    dataItem,
 }: {
     close: () => void;
-    dataItem: Movie;
 }) {
 
-    const dispatch = useDispatch();
   const selectedMovie = useSelector((state: RootState) => state.movies.selectedMovie);
 
   if (!selectedMovie) return null;
@@ -20,15 +15,15 @@ export default function Details({
     return (
         <div className="w-screen h-screen bg-[#08020236] fixed top-0 left-0 flex justify-center items-center z-10" >
             <div className="bg-bg_light2 w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2 flex flex-col md:flex-row relative rounded-lg overflow-hidden max-h-[90vh]">
-                <img src={dataItem.Poster} alt="" className="object-cover w-full md:w-1/2 max-h-[50vh] md:max-h-full" />
+                <img src={selectedMovie.Poster} alt="" className="object-cover w-full md:w-1/2 max-h-[50vh] md:max-h-full" />
                 <div className="w-full md:w-1/2 flex flex-col items-center p-5">
                     <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold px-2 py-3 text-primary-color text-center">
                         Movie Info
                     </h1>
                     <div className="flex flex-col p-5 gap-3 text-base md:text-lg font-semibold">
-                        <span> Title: <span className="font-normal">{dataItem.Title}</span> </span>
-                        <span> Year: <span className="font-normal">{dataItem.Year}</span> </span>
-                        <span> Movie Type: <span className="font-normal">{dataItem.Type}</span> </span>
+                        <span> Title: <span className="font-normal">{selectedMovie.Title}</span> </span>
+                        <span> Year: <span className="font-normal">{selectedMovie.Year}</span> </span>
+                        <span> Movie Type: <span className="font-normal">{selectedMovie.Type}</span> </span>
 
                     </div>
 
@@ -38,7 +33,7 @@ export default function Details({
                         onClick={close}
                         className="absolute top-2 right-2 cursor-pointer w-8 h-8"
                     />
-                    <span className='font-thin text-sm text-primary-color'>cliquer sur echap pour fermer</span>
+                    <span className='font-mono text-lg text-primary-color mt-10'>cliquer sur echap pour fermer</span>
                 </div>
             </div>
         </div>
--- src/components/shared/movieCard.tsx
+++ src/components/shared/movieCard.tsx
@@ -28,7 +28,7 @@ export default function MovieCard({ props }: { props: Movie }) {
   return (
     <div className='h-[300px] w-[200px] rounded-lg relative mb-4 bg-bg_light2' onClick={() => setIsOpen(true)}>
       <img src={props.Poster} alt="" className='h-[80%] w-full rounded-lg' />
-      <div className='flex items-center justify-between px-3'>
+      <div className='flex items-center justify-between px-3 mt-2'>
         <span className='font-semibold truncate' >{props.Title} </span>
         <span className='p-1 bg-primary-color rounded-md text-white' >{props.Year} </span>
       </div>
