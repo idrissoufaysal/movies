990dbbfbf8131eb95e02d56721357a13d42e2d2c
idrissou <idrissoufaysal93@gmail.com>
2024-10-29 08:07:07 +0000

    a

--- src/components/shared/movieCard.tsx
+++ src/components/shared/movieCard.tsx
@@ -25,7 +25,7 @@ export default function MovieCard({ props }: { props: Movie }) {
   }, [])
 
   return (
-    <div className='h-[300px] w-[200px] rounded-lg relative mb-4 bg-bg_light2' onClick={() => setIsOpen(true)}>
+    <div className='h-[300px] w-[200px] rounded-lg relative mb-4 bg-bg_light2 cursor-pointer' onClick={() => setIsOpen(true)}>
       <img src={props.Poster} alt="" className='h-[80%] w-full rounded-lg' />
       <div className='flex items-center justify-between px-3 mt-2'>
         <span className='font-semibold truncate' >{props.Title} </span>
